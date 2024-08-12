99ca9173ec65fd6386218f25ce187c4ff6bbbd8f
1Tomate65 <nicklasniehm@gmail.com>
2024-08-13 00:19:52 +0200

    Very Important Changes

--- src/main/java/net/tomate65/underworld_skies/Underworld_skies.java
+++ src/main/java/net/tomate65/underworld_skies/Underworld_skies.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// very important comment
 public class Underworld_skies implements ModInitializer {
 	public static final String MOD_ID = "underworld_skies";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
